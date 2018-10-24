f23faa299c25fd12a5362587bd23b81d45fe3279
m.smolina <m-smolina@yandex-team.ru>
2018-10-24 15:26:46 +0300

    Initial commit

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/ping")
+    public String index() {
+        return "pong";
+    }
+
+}
